944239e0a5ba16ba2a089f20281a75e71739609f
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-12-15 15:20:42 +0100

    Added connection string to user secrets

--- DatabaseModel/BookstoreContext.cs
+++ DatabaseModel/BookstoreContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace SQLLab2;
@@ -43,9 +44,10 @@ public partial class BookstoreContext : DbContext
     public virtual DbSet<TitlesPerAuthor> TitlesPerAuthors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-    { 
-        optionsBuilder.UseSqlServer("Initial Catalog=Bookstore;Integrated Security=True;Trust Server Certificate=True;Server SPN=localhost");
+    {
+        var config = new ConfigurationBuilder().AddUserSecrets<BookstoreContext>().Build();
+        var connectionString = config["ConnectionString"];
+        optionsBuilder.UseSqlServer(connectionString);
         AllocConsole();
         optionsBuilder
             .LogTo(
